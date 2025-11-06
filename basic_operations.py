857cfa1ae01c775cf0dc18664817e9012a986be6
newel <tsogbenewel@gmail.com>
2025-11-06 20:38:46 +0000

    Correct python file

---
+++ basic_operations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Define numbers
+number1 = 10
+number2 = 5
+
+# Perform addition on number1 and number2
+sum_result = number1 + number2
+
+#Printing the result
+print(f"Addition of {number1} and {number2} is {sum_result}")
+
+# Perform substraction on number1 and number2
+difference_result = number1 - number2
+
+#Printing the result
+print(f"Substraction of {number1} and {number2} is {difference_result}")
+
+# Perform multiplication on number1 and number2
+product_result = number1 * number2
+
+# Printing result
+print(f"Multiplication of {number1} and {number2} is {product_result}")
