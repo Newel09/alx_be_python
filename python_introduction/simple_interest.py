be5ff292db91e67ba0b0c0873f3b25c9de22f0e0
Newel09 <tsogbenewel@gmail.com>
2025-11-06 23:49:17 -0800

    simple interest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Defining the variables
+
+principal = 1000
+
+rate = 0.05
+
+time = 3
+
+# Calculating the simple interest
+
+simple_interest = principal * rate * time
+
+# Printing the result
+
+print(f"The simple interest is: {simple_interest}")
