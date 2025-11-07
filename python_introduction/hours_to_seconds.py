8589e49062a27c29825a2a5a703b545620dac588
newel <tsogbenewel@gmail.com>
2025-11-07 22:25:40 +0000

    converting hours to seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Task Description:
+
+1. For this task, write a Python script that converts a specific number of hours into seconds. 
+2. This task reinforces the concept of arithmetic operations within a practical context.
+
+"""
+# Defining variables
+one_hour = 60
+one_minute = 60
+
+# Converting 2 hours into seconds
+two_hours = one_hour * one_minute * 2
+
+print(f"2 hour(s) is {two_hours} seconds.")
